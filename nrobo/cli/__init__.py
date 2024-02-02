bdbeb9a96954ce4bc657ff1eda029678367df1f0
Panchdev Singh Chauhan <erpanchdev@gmail.com>
2024-02-02 17:03:04 +0530

    refactor code

--- nrobo/cli/__init__.py
+++ nrobo/cli/__init__.py
@@ -4,6 +4,9 @@ install command line utility of nrobo-copy framework.
 from nrobo.util.process import run_command
 from nrobo.util.python import verify_set_python_command
 
+# refer to global defined in nrobo.util.process
+global __PYTHON__
+
 
 def install_requirements(requirements_file):
     """
@@ -11,7 +14,7 @@ def install_requirements(requirements_file):
     :return:
     """
     try:
-        run_command("pip3 install -r {}".format(requirements_file))
+        run_command(__PYTHON__ + " install -r {}".format(requirements_file))
     except Exception as e:
         print(e)
 
--- nrobo/util/process/__init__.py
+++ nrobo/util/process/__init__.py
@@ -1,5 +1,6 @@
 """
 subprocess doc: https://docs.python.org/3/library/subprocess.html
+another subprocess doc: https://www.geeksforgeeks.org/python-subprocess-module/
 
 """
 import os, subprocess as sp
