6bc10aea974ade938984373a47f231eed6faf396
Panchdev Singh Chauhan <erpanchdev@gmail.com>
2024-02-11 20:20:49 +0530

    moved run_unit_tests metho under if condition

--- validate_nrobo.py
+++ validate_nrobo.py
@@ -1,5 +1,15 @@
 import subprocess
 from nrobo import set_environment, Environment, EnvKeys
-set_environment()
-# pytest nrobo_framework_tests --noconftest --confcutdir nrobo_framework_tests
-subprocess.check_call(['pytest', 'nrobo_framework_tests', '--noconftest', '--noconftest', 'nrobo_framework_tests', '-n', '10'])
+from nrobo.util.process import terminal
+
+
+def run_unit_tests() -> int:
+    """run nrobo framework unit tests"""
+    set_environment()
+    # pytest nrobo_framework_tests --noconftest --confcutdir nrobo_framework_tests
+    return terminal(
+        ['pytest', 'nrobo_framework_tests', '--noconftest', '--noconftest', 'nrobo_framework_tests', '-n', '10'])
+
+
+if __name__ == '__main__':
+    run_unit_tests()
