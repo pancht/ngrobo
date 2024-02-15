904d9a78841134e78aa32083cf84e1c6ed5dde5a
Panchdev Singh Chauhan <erpanchdev@gmail.com>
2024-02-15 09:15:46 +0530

    added doc string

--- cli/check/__init__.py
+++ cli/check/__init__.py
@@ -1,19 +1,30 @@
+"""
+=====================CAUTION=======================
+DO NOT DELETE THIS FILE SINCE IT IS PART OF NROBO
+FRAMEWORK AND IT MAY CHANGE IN THE FUTURE UPGRADES
+OF NROBO FRAMEWORK. THUS, TO BE ABLE TO SAFELY UPGRADE
+TO LATEST NROBO VERSION, PLEASE DO NOT DELETE THIS
+FILE OR ALTER ITS LOCATION OR ALTER ITS CONTENT!!!
+===================================================
+
+This module has actions pertaining to nRoBo verifying packages.
+
+@author: Panchdev Singh Chauhan
+@email: erpanchdev@gmail.com
+"""
 import os
 
 from nrobo.util.process import terminal
 from nrobo.util.platform import PLATFORMS
 from nrobo import *
 
-def check(debug=False):
-    """
-    Check package before upload
 
-    :return:
-    """
+def check(debug=False) -> None:
+    """Checks packages before publishing."""
+
     terminal([os.environ[EnvKeys.PYTHON], "-m", "pip", "install", "twine"])
     terminal([os.environ[EnvKeys.PYTHON], "-m", "pip", "install", "--upgrade", "twine"])
     if os.environ[EnvKeys.HOST_PLATFORM] in [PLATFORMS.DARWIN, PLATFORMS.LINUX, PLATFORMS.MACOS]:
         terminal(["twine", "check", "dist" + os.sep + "*"], debug=debug)
     elif os.environ[EnvKeys.HOST_PLATFORM] in [PLATFORMS.WINDOWS]:
-        terminal(["twine", "check", "."+os.sep+"dist"+os.sep+"*.*"], debug=debug)
-
+        terminal(["twine", "check", "." + os.sep + "dist" + os.sep + "*.*"], debug=debug)
