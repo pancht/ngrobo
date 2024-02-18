88d50a10c6de3816aa305ef98cf69f64c6bb86e8
Panchdev Singh Chauhan <erpanchdev@gmail.com>
2024-02-18 07:21:08 +0530

    add module doc

--- nrobo/util/version/__init__.py
+++ nrobo/util/version/__init__.py
@@ -1,3 +1,18 @@
+"""
+=====================CAUTION=======================
+DO NOT DELETE THIS FILE SINCE IT IS PART OF NROBO
+FRAMEWORK AND IT MAY CHANGE IN THE FUTURE UPGRADES
+OF NROBO FRAMEWORK. THUS, TO BE ABLE TO SAFELY UPGRADE
+TO LATEST NROBO VERSION, PLEASE DO NOT DELETE THIS
+FILE OR ALTER ITS LOCATION OR ALTER ITS CONTENT!!!
+===================================================
+
+Version class handles version operations smoothly.
+
+
+@author: Panchdev Singh Chauhan
+@email: erpanchdev@gmail.com
+"""
 from nrobo.exceptions import NRoBoIncorrectVersion, NRoBoInvalidOperation
 
 
