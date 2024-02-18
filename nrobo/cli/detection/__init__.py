c00d97acce73361de740bab86ed2fd360853dcec
Panchdev Singh Chauhan <erpanchdev@gmail.com>
2024-02-18 09:35:51 +0530

    added doc

--- nrobo/cli/detection/__init__.py
+++ nrobo/cli/detection/__init__.py
@@ -1,3 +1,19 @@
+"""
+=====================CAUTION=======================
+DO NOT DELETE THIS FILE SINCE IT IS PART OF NROBO
+FRAMEWORK AND IT MAY CHANGE IN THE FUTURE UPGRADES
+OF NROBO FRAMEWORK. THUS, TO BE ABLE TO SAFELY UPGRADE
+TO LATEST NROBO VERSION, PLEASE DO NOT DELETE THIS
+FILE OR ALTER ITS LOCATION OR ALTER ITS CONTENT!!!
+===================================================
+
+Detection module has methods to detect environment,
+nRoBo installation and many more...
+
+
+@author: Panchdev Singh Chauhan
+@email: erpanchdev@gmail.com
+"""
 import os
 from pathlib import Path
 
