f744da5d76397f1c8564bb64b96a0c9e57548d9e
Panchdev Singh Chauhan <erpanchdev@gmail.com>
2024-03-07 13:55:30 +0530

    adjusted a test and restructured sample tests

--- nrobo/framework/tests/mobile/appium/test_appium.py
+++ nrobo/framework/tests/mobile/appium/test_appium.py
@@ -1,17 +1,16 @@
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
 
-from nrobo.framework.pages import Page
+from pages import Page
 
 
 class TestAppium:
-    """Sample mobile tests using appium open source library"""
 
     def test_namastey_appium_world(self, driver, logger):
         """First appium test"""
         logger.info("Create page object")
         page = Page(driver=driver, logger=logger)
-        page.click(by=AppiumBy.XPATH, value='//*[@text="Battery"]')
+        #page.click(by=AppiumBy.XPATH, value='//*[@text="Battery"]')
 
 
 
